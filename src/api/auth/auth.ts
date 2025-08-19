2d2a451f9e2300ee325427cfb990d7bd683abe94
nickel93 <bellchaeni@gmail.comgit config --global user.name nickel93git config --global user.email bellchaeni@gmail.com>
2025-08-20 01:16:14 +0900

    NT 테스트 url

--- src/api/auth/auth.ts
+++ src/api/auth/auth.ts
@@ -4,8 +4,13 @@ import Kakao from "next-auth/providers/kakao";
 export const authConfig = {
   providers: [
     Kakao({
-      clientId: process.env.KAKAO_CLIENT_ID!,
-      clientSecret: process.env.KAKAO_CLIENT_SECRET ?? "",
+      clientId:
+        process.env.KAKAO_CLIENT_ID || "34678491f4ad828e79d6d36160c63fc1",
+      clientSecret:
+        process.env.KAKAO_CLIENT_SECRET || "8y9bP0KydIR5FQQD1bioht0FExCEZXx2",
+      authorization: {
+        url: "http://dinning-together.vercel.app",
+      },
     }),
   ],
   callbacks: {
