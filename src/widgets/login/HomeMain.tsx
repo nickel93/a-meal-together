3ddd8802d73bfe952f88ce8e473cccb6bb70e24e
nickel93 <bellchaeni@gmail.comgit config --global user.name nickel93git config --global user.email bellchaeni@gmail.com>
2025-08-26 23:26:09 +0900

    헤더 아이콘 추가

--- src/widgets/login/HomeMain.tsx
+++ src/widgets/login/HomeMain.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+import { SurveryLogo } from "@/icon";
+
 const HomeMain = () => {
   return (
     <main className="flex flex-col items-center pt-[120px]">
+      <SurveryLogo />
       <h1 className="font-normal text-[40px]">가치한상</h1>
       <p>‘같이’ 먹고, ‘가치’ 있게 즐기는 한상 모임</p>
     </main>
