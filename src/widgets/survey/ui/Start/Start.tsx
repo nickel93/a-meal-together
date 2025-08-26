97190c15dc5bb3e66178778b8c1e27a106ec938d
nickel93 <bellchaeni@gmail.comgit config --global user.name nickel93git config --global user.email bellchaeni@gmail.com>
2025-08-26 23:54:51 +0900

    NT 테스트 콘솔 삭제

--- src/widgets/survey/ui/Start/Start.tsx
+++ src/widgets/survey/ui/Start/Start.tsx
@@ -3,12 +3,10 @@ import { SurveyButton } from "@/feature/survey";
 import SurveyHeader from "./Header";
 import SurveyMain from "./Main";
 import { useRouter } from "next/navigation";
-import { useSession } from "next-auth/react";
 
 const SurveyStart = () => {
   const { push } = useRouter();
-  const { data: session } = useSession();
-  console.log("session", session);
+
   return (
     <>
       <div className="flex flex-col items-start gap-[40px] h-full">
